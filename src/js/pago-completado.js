edcc770ce585c69962f1a500eead0111386ca787
JacielJM <jacimenera@gmail.com>
2022-09-08 22:22:44 -0500

    se anade regex para validar tarjeta

--- src/js/pago-completado.js
+++ src/js/pago-completado.js
@@ -6,24 +6,27 @@ export function verifyPago() {
 
 	const valores = [cardNumber.value, cardName.value, cardExpiration.value];
 	let flag = valores.some(v => v == '');
+	let regex = /([0-9][ -]?){16}/;
 	if (!flag) {
-		alerta.innerHTML =
-			'<div class="spinner-border text-psgblue" role="status"><span class="visually-hidden">Loading...</span></div>';
-		setTimeout(() => {
-			alerta.setAttribute(
-				'class',
-				'bg-transparent rounded text-dark mt-2 py-1 text-center'
-			);
+		if (regex.test(valores[0])) {
 			alerta.innerHTML =
-				'Datos validados correctamente <iconify-icon icon="flat-color-icons:ok"></iconify-icon>';
-		}, 2500);
+				'<div class="spinner-border text-psgblue" role="status"><span class="visually-hidden">Loading...</span></div>';
+			setTimeout(() => {
+				alerta.setAttribute(
+					'class',
+					'bg-transparent rounded text-dark mt-2 py-1 text-center'
+				);
+				alerta.innerHTML =
+					'Datos validados correctamente <iconify-icon icon="flat-color-icons:ok"></iconify-icon>';
+			}, 2500);
+		}
 	} else {
 		alerta.setAttribute(
 			'class',
 			'bg-transparent rounded text-dark mt-2 py-1 text-center'
 		);
 		alerta.innerHTML =
-			'Por favor complete sus datos <iconify-icon icon="bxs:error" style="color: orange;"></iconify-icon>';
+			'Por favor complete sus datos correctamente <iconify-icon icon="bxs:error" style="color: orange;"></iconify-icon>';
 	}
 	return !flag;
 }
