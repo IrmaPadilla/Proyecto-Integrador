cbb35ba330948aa9760f53768daf623dabeb631a
JacielJM <jacimenera@gmail.com>
2022-09-11 14:12:41 -0500

    opt de regex p validacion de numero de tarjeta

--- src/js/pago-completado.js
+++ src/js/pago-completado.js
@@ -5,22 +5,25 @@ export function verifyPago() {
 	const cardExpiration = document.getElementById('cardExpiration');
 
 	const valores = [cardNumber.value, cardName.value, cardExpiration.value];
+
 	let flag = valores.some(v => v == '');
-	let regex = /([0-9][ -]?){16}/;
-	if (!flag) {
-		if (regex.test(valores[0])) {
+
+	let regex = /\b([0-9][ -]?){16}(?![\w -])\b/; // evalua el formato de los numeros de la tarjeta
+
+	if (!flag && regex.test(valores[0])) {
+		alerta.innerHTML =
+			'<div class="spinner-border text-psgblue" role="status"><span class="visually-hidden">Loading...</span></div>';
+		setTimeout(() => {
+			alerta.setAttribute(
+				'class',
+				'bg-transparent rounded text-dark mt-2 py-1 text-center'
+			);
 			alerta.innerHTML =
-				'<div class="spinner-border text-psgblue" role="status"><span class="visually-hidden">Loading...</span></div>';
-			setTimeout(() => {
-				alerta.setAttribute(
-					'class',
-					'bg-transparent rounded text-dark mt-2 py-1 text-center'
-				);
-				alerta.innerHTML =
-					'Datos validados correctamente <iconify-icon icon="flat-color-icons:ok"></iconify-icon>';
-			}, 2500);
-		}
+				'Datos validados correctamente <iconify-icon icon="flat-color-icons:ok"></iconify-icon>';
+		}, 2000);
+		flag = false;
 	} else {
+		flag = true;
 		alerta.setAttribute(
 			'class',
 			'bg-transparent rounded text-dark mt-2 py-1 text-center'
