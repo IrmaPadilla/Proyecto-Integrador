1a67bfc29b14f0b214ddd0f2c4a283ed141be500
juanCCM57 <jcarreramendoza45@gmail.com>
2022-09-20 12:35:45 -0500

    Siuuuuuu

--- src/js/productossegundamano.js
+++ src/js/productossegundamano.js
@@ -1,70 +1,98 @@
-const recienllegados = document.querySelector('#recienllegados');
 const tarjeta = document.querySelector('#tarjeta');
+const cards = document.querySelector('#cards');
+const productos = document.querySelector('#productos');
+const recienllegados = document.querySelector('#recienllegados');
+
 let segundamano = [];
 let segundamano2 = [];
-const Productoss = async () => {
-    try{
-        const res = await fetch('../src/js/segundamano.json');
-        segundamano = await res.json();
-        mostrarproductos(segundamano);
-    } catch(e){
-        console.log(e);
-    }
-}
-const Productoss2 = async () => {
-    try{
-        const res = await fetch('../src/js/segundamano2.json');
-        segundamano2 = await res.json();
-        mostrarproductos(segundamano2);
-    } catch(e){
-        console.log(e);
-    }
-}
 
-const mostrarproductos = (segundamano) =>{
-segundamano.forEach( card => {
+// SearchBar
+const SearchBar = document.querySelector('#buscador');
+
+SearchBar.addEventListener('input', (e) => {
+  const searchString = e.target.value.toLowerCase();
+  const filteredProducts = segundamano.filter((product) => {
+    return (
+      product.nombre.toLowerCase().includes(searchString) ||
+      product.descrpcion.toLowerCase().includes(searchString)
+    );
+  });
+  mostrarproductos(filteredProducts);
+});
+SearchBar.addEventListener('input', (e) => {
+  const searchString2 = e.target.value.toLowerCase();
+  const filteredProducts2 = segundamano2.filter((item) => {
+    return (
+      item.nombre.toLowerCase().includes(searchString2) ||
+      item.descrpcion.toLowerCase().includes(searchString2)
+    );
+  });
+  mostrarproductos2(filteredProducts2);
+});
+
+const Productoss = async () => {
+  try {
+    const res = await fetch('../src/js/segundamano.json');
+    segundamano = await res.json();
+    mostrarproductos(segundamano);
+  } catch (e) {
+    console.log(e);
+  }
+};
+const Productoss2 = async () => {
+  try {
+    const res = await fetch('../src/js/segundamano2.json');
+    segundamano2 = await res.json();
+    mostrarproductos2(segundamano2);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const mostrarproductos = (segundamano) => {
+  limpiarHTML();
+  segundamano.forEach((card) => {
     const elementos = document.createElement('div');
-    elementos.classList.add(
-        'row', 
-        'row-cols-1', 
-        'row-cols-md-4', 
-        'mx-2', 
-        'px-2', 
-        'g-2',
-    ); 
-     elementos.innerHTML +=` 
-     <div class="content col card">
+    elementos.classList.add('content', 'col', 'card');
+
+    elementos.innerHTML += `
      <img  class="imgall" src=${card.imagen}>
      <h3>${card.nombre}</h3>
-     <p>${card.descrpcion}</p>
+     <p class="descripcion">${card.descrpcion}</p>
      <h6>${card.precio}</h6>
-     <a href="#" class="btn btn-light float-right cardbutton"">Añadir al carro>></a>
-    </div>`
-tarjeta.appendChild(elementos);
-});
-}
+     <a href="#" class="btn btn-light float-right cardbutton">Añadir al carro>></a>
+    </div>`;
+    productos.appendChild(elementos);
+  });
+};
 
-const mostrarproductos2 = (segundamano2) =>{
-    segundamano2.forEach( card => {
-        const elementos = document.createElement('div');
-        elementos.classList.add(
-            'row', 
-            'row-cols-1', 
-            'row-cols-md-4', 
-            'mx-2', 
-            'px-2', 
-            'g-2',
-        ); 
-         elementos.innerHTML +=` 
+const mostrarproductos2 = (segundamano2) => {
+  limpiarHTML2();
+  segundamano2.forEach((card) => {
+    const elementos = document.createElement('div');
+    elementos.classList.add('content', 'col', 'card');
+    elementos.innerHTML += `
          <div class="content col card">
          <img  class="imgall" src=${card.imagen}>
          <h3>${card.nombre}</h3>
-         <p>${card.descrpcion}</p>
+         <p class="descripcion">${card.descrpcion}</p>
          <h6>${card.precio}</h6>
          <a href="#" class="btn btn-light float-right cardbutton"">Añadir al carro>></a>
-        </div>`
-        recienllegados.appendChild(elementos);
-    });
-    }
-    Productoss();
-    Productoss2();
+        </div>`;
+    recienllegados.appendChild(elementos);
+  });
+};
+Productoss();
+Productoss2();
+
+function limpiarHTML2() {
+  while (recienllegados.firstChild) {
+    recienllegados.removeChild(recienllegados.firstChild);
+  }
+}
+
+function limpiarHTML() {
+  while (productos.firstChild) {
+    productos.removeChild(productos.firstChild);
+  }
+}
