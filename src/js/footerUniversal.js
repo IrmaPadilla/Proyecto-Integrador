da9f9dfa487c2fb391d233705d4fe36a405c7023
Laura Sánchez <lnsz.software@gmail.com>
2022-09-20 11:47:43 -0500

    Se prepara estructura para consumo de API, con metodo POST.

--- src/js/footerUniversal.js
+++ src/js/footerUniversal.js
@@ -1,3 +1,12 @@
+const URL_MAIN = "http://localhost:8080/api/newsletter";
+
+let form = document.getElementById('newsForm');
+
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+  console.log("clic done")
+});
+
 copyrightDate();
 function copyrightDate() {
   const year = new Date();
@@ -6,3 +15,18 @@ function copyrightDate() {
     ${year.getFullYear()} Copyright:
   `;
 }
+
+fetch(URL_MAIN, { //URL del servicio a donde se hara el POST
+  method: 'POST', // or 'PUT' 
+  headers: { // se agrega el header
+    'Content-Type': 'application/json', //tipo de contenido
+  },
+  body: JSON.stringify(data), //se agrega el cuerpo del POST
+})
+.then(response => response.json()) //se obtiene la respuesta del servidor
+.then(data => { //se obtiene el json
+  console.log('Success:', data); //se imprime el json
+})
+.catch((error) => { //si hay un error
+  console.error('Error:', error); //se imprime el error
+});
