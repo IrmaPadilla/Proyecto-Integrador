abf92cb70f728082546e35479340768e6655847b
JacielJM <jacimenera@gmail.com>
2022-09-21 22:46:26 -0500

    preparacion para fetch API

--- src/js/getTicket.js
+++ src/js/getTicket.js
@@ -6,13 +6,27 @@ export function getInfo(value) {
 
 	const fields = [clientName, purchaseDate, email, phone];
 
+	// const URL = 'http://localhost:8080/api/garantias/';
+
 	let flag = false;
 
 	let regex = /\b[0-9]{10}(?![\w -])\b/;
 
-	regex.test(value)
-		? (fields.forEach(field => field.removeAttribute('disabled')),
-		  (flag = true))
-		: fields.forEach(field => field.setAttribute('disabled', 'true'));
+	if (regex.test(value)) {
+		fields.forEach(field => field.removeAttribute('disabled'));
+		flag = true;
+		fetch(URL, { method: 'get' })
+			.then(response => response.json())
+			.then(json => completarDatos(json))
+			.catch(error => console.log(error));
+	} else {
+		fields.forEach(field => field.setAttribute('disabled', 'true'));
+	}
+	const completarDatos = json => {
+		clientName.value = json.cliente;
+		purchaseDate.value = json.fecha;
+		email.value = json.correo;
+		phone.value = json.telefono;
+	};
 	return flag;
 }
