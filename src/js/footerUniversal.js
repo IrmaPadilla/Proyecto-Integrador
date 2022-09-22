2fcb7a2395467efa28ec621f3c73012b2ac27768
Laura Sánchez <lnsz.software@gmail.com>
2022-09-22 00:44:30 -0500

    Fetch en Newsletter funcional.

--- src/js/footerUniversal.js
+++ src/js/footerUniversal.js
@@ -1,5 +1,3 @@
-const URL_MAIN = "http://localhost:8080/api/newsletter";
-
 copyrightDate();
 function copyrightDate() {
   const year = new Date();
@@ -12,25 +10,22 @@ function copyrightDate() {
 document.getElementById("subscribe").addEventListener("click", function (e) {
   e.preventDefault();
   console.log("clic done")
-  // let data = new FormData(form);
-  // console.log(data);
-  // console.log(data.get('email'));
   let email = document.getElementById("email");
   console.log(email.value);
   const data = {
     correo: email.value,
   };
 
-  fetch(URL_MAIN, {
-    method: "POST", // or 'PUT'
+  fetch("http://localhost:8080/api/newsletter/", {
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(data), //Aqui estoy llamando a mi cuerpo de la solicitud.
+    body: JSON.stringify(data), // cuerpo de la solicitud.
   })
     .then((response) => response.text())
     .then((data) => {
-      console.log("Correo Guardado:", data); //Mensaje para cuando se agreguen los datos correctamente
+      console.log("Correo Guardado:", data); 
     })
     .catch((error) => {
       console.error("Error:", error);
