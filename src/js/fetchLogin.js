22d3087be912d3576ff95284cdc97ce3e2e953b3
Laura Sánchez <lnsz.software@gmail.com>
2022-09-23 10:19:56 -0500

    Final fetch

--- src/js/fetchLogin.js
+++ src/js/fetchLogin.js
@@ -1,14 +1,13 @@
 document.getElementById("btnSend").addEventListener("click", function(e){
     let nombreUsuario = document.getElementById("nombreUsuario");
     let contraseña = document.getElementById("contraseña");
-    console.log(username.value);
-    console.log(password.value);
-    const data = {nombreUsuario:nombreUsuario.value, 
-                        contraseña: contraseña.value
+    const data = {
+        nombreUsuario:nombreUsuario.value, 
+        contraseña: contraseña.value
     };
 
-fetch("http://localhost:8080/api/login/", {  // Falta cambiar esta URL a la verdadera q estemos usando
-method: 'POST', // o 'PUT'
+fetch("http://localhost:8080/api/clientes/", {
+method: 'POST',
 headers: {
 'Content-Type': 'application/json',
 },
--- src/js/fetchRegister.js
+++ src/js/fetchRegister.js
@@ -5,18 +5,21 @@ document.getElementById("btnSend").addEventListener("click", function(e){
     let nombreUsuario = document.getElementById("nombreUsuario");
     let contraseña = document.getElementById("contraseña");
     let correo = document.getElementById("correo");
-    console.log(username.value);
-    console.log(password.value);
+    let newsletter = document.getElementById("newsletter").checked;
+    // console.log(nombreUsuario.value);
+    // console.log(contraseña.value);
+    // console.log(newsletter)
     const data = { 
-        nombreUsuario: nombreUsuario.value, 
-        contraseña: contraseña.value,
         nombre: nombre.value,
-        apellido: apellido.value,
+        apellido: apellido.value, 
+        contraseña: contraseña.value,
+        correo: correo.value,
         telefono: telefono.value,
-        correo: correo.value
+        nombreUsuario: nombreUsuario.value,
+        newsletter: newsletter
     };
-
-fetch("http://localhost:8080/api/register/", { // Falta cambiar esta URL a la verdadera q estemos usando
+    
+fetch("http://localhost:8080/api/clientes/", { // Falta cambiar esta URL a la verdadera q estemos usando
 method: 'POST', // o 'PUT'
 headers: {
 'Content-Type': 'application/json',
